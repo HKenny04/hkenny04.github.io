bc57f43f4061e7c59ccbf5cf4365cf88d45f01a8
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2020-01-30 21:39:54 -0500

    about section gucci

--- about-kenny/src/App.js
+++ about-kenny/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar.js';
 import About from './components/About.js';
-import Profile from './components/Profile.js';
 import Skills from './components/Skills.js';
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <About id="about" />
-        <Profile id="profile" />
         <Skills id="skills" />
       </div>
     );
--- about-kenny/src/components/About.js
+++ about-kenny/src/components/About.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import profile from '../images/linkedin.jpg';
 
 class About extends Component {
   render() {
     return (
         <div id="about">
-          <p>Hey there, my name is Kenny Hu. I'm an undergraduate student studying Computer Science and Applied Mathematics at Stony Brook University.</p>
-          <br/>
-          <p>During my free time I like to workout, watch TV, look at memes, play games, and do some coding.</p>
+          <div className="split left">
+            <div className="intro">
+              Hey there, my name is Kenny Hu. I'm an undergraduate student studying Computer Science and Applied Mathematics at Stony Brook University.<br/>
+              During my free time I like to workout, watch TV, look at memes, play games, and do some coding.
+            </div>
+          </div>
+          <div className="split right">
+            <img alt={"profile"} src={profile} /><br/><br/>
+            <h2>Kenny Hu</h2>
+            <h4>Junior at Stony Brook University</h4>
+            <h5>Computer Science, B.S.</h5>
+            <h5>Applied Mathematics and Statistics, B.S.</h5>
+          </div>
         </div>
     );
   };
